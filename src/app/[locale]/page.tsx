8a4bf0e8e2ea9a975ba6d624210644472955cbca
WeulerSilva <weulergamer@outlook.com>
2024-10-15 21:21:22 -0300

    add video on the product part

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -71,19 +71,37 @@ export default function HomePage() {
 
 
 
-      <div className='w-screen h-[450px] bg-products-mobile bg-[#F0F0F0] mt-16 relative bg-contain bg-no-repeat bg-bottom uppercase
-              md:bg-center md:h-[400px] lg:h-[450px] md:bg-products' title={t('products-legend')}>
-                <div className='hidden w-[20%] h-[15%] bg-logo-cert bg-center bg-contain bg-no-repeat absolute 
+      <div className='w-screen h-[400px] bg-[#F0F0F0] mt-16 relative flex justify-center items-center uppercase
+              md:bg-center md:h-[400px] lg:h-[450px]' title={t('products-legend')}>
+        <video src="/images/products-bg-mobile.mp4"
+          className='w-full h-full object-contain absolute pt-24 md:hidden'
+          autoPlay
+          loop
+          muted
+          playsInline
+        >
+        </video>
+
+        <video src="/images/products-bg.mp4"
+          className='w-full h-full object-fill hidden absolute md:block '
+          autoPlay
+          loop
+          muted
+          playsInline
+        >
+        </video>
+
+        <div className='hidden w-[20%] h-[15%] bg-logo-cert bg-center bg-contain bg-no-repeat absolute 
                 top-[4%] right-[2%] md:block lg:top-[8%] lg:right-[3%] lg:w-[15%] 2xl:right-[9.5%]'></div>
         <div className='w-full h-full flex justify-start items-center flex-col md:justify-center pt-4 md:pt-6
               md:items-start md:flex-row lg:pt-12'>
-          <h2 className='text-[27px] font-bold font-renogare text-redclear' id='products'>
+          <h2 className='text-[27px] font-bold font-renogare text-redclear relative' id='products'>
             {t('products')}
           </h2>
-          <a className='bg-browndark text-[16px] font-renogare px-3 py-1 rounded-3xl text-white mt-1
+          <a className='bg-browndark text-[16px] relative font-renogare px-3 py-1 rounded-3xl text-white mt-1
               ml-4' href='https://ank-ilumer.com/' target='_blank' title={t('more-legend')}>
-                {t('more')}
-              </a>
+            {t('more')}
+          </a>
         </div>
 
       </div>
@@ -97,24 +115,24 @@ export default function HomePage() {
             <div className='w-[20%] h-full bg-icon-1 bg-contain bg-center bg-no-repeat'></div>
             <h6 className='w-[40%] h-full flex justify-center items-center text-[20px] font-medium text-white 
                 md:w-[40%] md:text-[16px] xl:text-[27px] xl:w-[38%]'>
-                  {t('serv-1')}</h6>
+              {t('serv-1')}</h6>
           </div>
           <div className='w-full h-[130px] bg-redclear flex justify-center items-center space-x-5 md:h-[70%] xl:h-full'>
             <div className='w-[20%] h-full bg-icon-2 bg-contain bg-center bg-no-repeat'></div>
             <h6 className='w-[40%] h-full flex justify-center items-center text-[20px] font-medium text-white 
                 md:w-[50%] md:text-[16px] xl:text-[27px] xl:w-[40%]'>
-                  {t('serv-2')}</h6>
+              {t('serv-2')}</h6>
           </div>
           <div className='w-full h-[130px] bg-browndark flex justify-center items-center space-x-5 md:h-[70%] xl:h-full'>
             <div className='w-[20%] h-full bg-icon-3 bg-contain bg-center bg-no-repeat'></div>
             <h6 className='w-[40%] h-full flex justify-center items-center text-[20px] font-medium text-white 
                   md:w-[50%] md:text-[16px] xl:text-[27px] xl:w-[48%] 2xl:w-[44%]'>
-                    {t('serv-3')}</h6>
+              {t('serv-3')}</h6>
           </div>
         </div>
       </section>
 
-      <EmailContact/>
+      <EmailContact />
 
     </>
   );
