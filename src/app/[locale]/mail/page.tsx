cda7436a58850b57894ac0e30b8e482084cf4f3b
programadorWBusiness <77157077+programadorWBusiness@users.noreply.github.com>
2024-12-16 23:08:42 -0300

    add file

---
+++ src/app/[locale]/mail/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function HomePage() {
+    return (
+        <>
+        <Image alt="" src={"/images/email-ilumer/ank/mail/anki-logo.png"} width={100} height={100}/>
+        <Image alt="" src={"/images/email-ilumer/ank/mail/web-icon.png"} width={100} height={100}/>
+        <Image alt="" src={"/images/email-ilumer/ilumer/mail/ilumer.png"} width={100} height={100}/>
+        <Image alt="" src={"/images/email-ilumer/ilumer/mail/logo_ilumer.png"} width={100} height={100}/>
+        <Image alt="" src={"/images/email-ilumer/waveseg/mail/instagram.png"} width={100} height={100}/>
+        <Image alt="" src={"/images/email-ilumer/waveseg/mail/seg-logo.png"} width={100} height={100}/>
+
+        </>
+    )
+}
